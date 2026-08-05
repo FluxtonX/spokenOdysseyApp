3a17ec462059013fb19e0d32628ed00cb5d52f1f
i-ikram <ikramudin1015@gmail.com>
2026-08-05 17:58:46 +0500

    remove old code

--- lib/config/app_config.dart
+++
@@ -1,3 +0,0 @@
-class AppConfigs {
-  static const String kAppId = "ae23c1ec704e446baa8a1196e7cfc953";
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:spokenodyssey/config/app_utils.dart';
-import 'package:spokenodyssey/views/splashScreen/splash_screen.dart';
-import 'package:spokenodyssey/firebase_options.dart';
-import 'config/get_it.dart';
-import 'controllers/auth_controller.dart';
-import 'controllers/navigation_controller.dart';
-import 'controllers/theme_controller.dart';
-import 'theme/theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await GetStorage.init();
-  await configureDependencies();
-  Get.put(AuthController(), permanent: true);
-  Get.put(ThemeController(), permanent: true);
-  Get.put(NavigationController(), permanent: true);
 
   runApp(const MyApp());
 }
@@ -28,21 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ThemeController themeController = Get.find<ThemeController>();
-
-    return Obx(
-      () => GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Spoken Odyssey',
-        theme: AppTheme.theme,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: themeController.themeMode.value,
-        home: const SplashScreen(),
-        builder: (context, child) {
-          CustomScreenUtil.init(context);
-          return child ?? const SizedBox.shrink();
-        },
-      ),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Spoken Odyssey',
+      home: Center(child: const Text('hello')),
     );
   }
 }
